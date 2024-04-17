16bba49d95398b678500b9afb1f2e101bf088e60
Talib Raeen <talibraeen13@gmail.com>
2024-04-17 16:06:34 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+var elem = document.querySelectorAll(".elem");
+
+elem.addEventListener("click", function(dets) {
+    console.log("click hua");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var ctr = document.getElementsByClassName("ctr")[0];
+// var main = document.getElementsByClassName("main")[0];
+// var loading = document.getElementsByClassName("loading")[0];
+// var load = document.getElementsByClassName("load")[0];
+// var btn = document.getElementsByClassName("btn")[0];
+
+// var flag = 0;
+
+// function useMe() {
+//     if (flag == 0) {
+        
+//         // console.log("shuru hua");
+//         flag = 1;
+//     }
+//     else {
+
+//         flag = 0;
+//         // console.log("band hua");
+//     }
+// }
